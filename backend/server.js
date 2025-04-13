06ab48e44e830c90d4a8a71f963a6ac573f00f8a
Dev-Rohan1 <mdrohanulhaquerohan368@gmail.com>
2025-04-13 23:35:08 +0600

    create backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
 
 import connectDB from "./src/db/connectDB.js";
 import webhookController from "./src/controllers/webhookController.js";
 import companyRoutes from "./src/routes/companyRoutes.js";
 import JobRoutes from "./src/routes/JobRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
 import Cloudinary from "./src/utils/cloudinary.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 connectDB();
 Cloudinary();
@@ -20,6 +23,7 @@ app.get("/", (req, res) => res.send("Api is working"));
 app.post("/webhook", webhookController);
 app.use("/company", companyRoutes);
 app.use("/job", JobRoutes);
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,13 @@
+export const getUserData = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const applyForJob = async (req, res) => {};
+
+export const getUserApplication = async (req, res) => {};
+
+export const updateUserResume = async () => {};
